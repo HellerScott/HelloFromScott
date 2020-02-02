a3775c9cd7449d0e653f3020983ea148d63cca4a
HellerScott <60409676+HellerScott@users.noreply.github.com>
2020-02-02 08:30:36 -0700

    Revert "Added line needed for part 8"

--- src/main.java
+++ src/main.java
@@ -1,6 +1,5 @@
 public class main {
   public static void run(String[] args) {
     System.out.println("Hello, my name is Scott.");
-    System.out.println("Hello Scott, my name is Brenton.");
   }
 }
