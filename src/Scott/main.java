fe1dc50b545e9e62a35b748bf0ee9ef34e8ef7f1
cuilei <lei18004@byui.edu>
2020-02-02 11:29:14 -0700

    change from Lisa

--- src/Scott/main.java
+++ src/Scott/main.java
@@ -1,5 +1,8 @@
+package Scott;
+
 public class main {
   public static void run(String[] args) {
     System.out.println("Hello, my name is Scott.");
+    System.out.println("Hello Scott, my name is Lisa.");
   }
 }
